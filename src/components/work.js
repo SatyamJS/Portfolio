6a55ace0c3c6d1c658a5f6098a2af208eff719d6
SATYAM GUPTA <73251754+SatyamJS@users.noreply.github.com>
2022-06-05 02:04:42 +0530

    Updated News site Domain Name

--- src/components/work.js
+++ src/components/work.js
@@ -10,7 +10,7 @@ class WorkComponent extends Component {
 
 
 
-                    <p data-aos="zoom-in"><a href="https://satyamg.w3spaces.com" target="_blank">Online news site</a><iframe scrolling="no" className="websiteFrame" src="https://satyamg.w3spaces.com" title="SgNews Website"></iframe></p>
+                    <p data-aos="zoom-in"><a href="https://satyamg.netlify.app" target="_blank">Online news site</a><iframe scrolling="no" className="websiteFrame" src="https://satyamg.w3spaces.com" title="SgNews Website"></iframe></p>
 
 
                 </div>
